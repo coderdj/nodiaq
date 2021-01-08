a5dc103c2f55b2287d3379783179d33d741b6742
Darryl Masson <darrylmasson@gmail.com>
2021-01-08 11:21:47 +0100

    Work

---
+++ public/javascripts/hypervisor.js
@@ -0,0 +1,41 @@
+
+function ControlBase(task, targets) {
+  $.ajax({
+    type: 'POST',
+    url: 'hypervisor/control',
+    data: {data: {task: task, targets: targets}},
+    success: () => {},
+    error: (jqXHR, text, error) => {alert("Error " + error + ": " + text);}
+  });
+}
+
+function VMEControl(crate, onoff) {
+}
+
+function RedaxCycle(reader, proc="reader") {
+  var targets = [];
+  if (reader == 'all') {
+    targets = ["reader0_reader_0","reader1_reader_0","reader2_reader_0","reader3_reader_0"];
+  } else {
+    targets = ["reader" + reader + "_" + proc + "_0"];
+  }
+  ControlBase("redax_ctl", targets);
+}
+
+function StartBootstrax(eb) {
+  var targets = [];
+  if (eb == 'all')
+    targets = ['eb0', 'eb1', 'eb3', 'eb4', 'eb5'];
+  else
+    targets = ['eb'+eb];
+  ControlBase('bootstraxctl_start', targets);
+}
+
+function StopBootstrax(eb) {
+  var targets = [];
+  if (eb == 'all')
+    targets = ['eb0', 'eb1', 'eb3', 'eb4', 'eb5'];
+  else
+    targets = ['eb'+eb];
+  ControlBase('bootstraxctl_stop', targets);
+}
--- routes/hypervisor.js
+++ routes/hypervisor.js
@@ -4,9 +4,11 @@ var router = express.Router();
 
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated() && typeof req.user.groups != 'undefined' && req.user.groups.includes('daq'))
-    return next();
-  return res.redirect('/login');
+  if (!req.isAuthenticated())
+    return res.redirect('/login');
+  if (typeof req.user.groups == 'undefined' || !req.user.groups.includes('daq'))
+    return res.redirect('/');
+  return next();
 }
 
 router.get('/', ensureAuthenticated, function(req, res) {
@@ -24,5 +26,35 @@ router.get('/vme_status', ensureAuthenticated, function(req, res) {
 });
 
 router.post('/control', ensureAuthenticated, function(req, res) {
-  data = req.body.data;
+  var data = req.body.data;
+  var commands = [];
+  var task = data.task;
+  for (var i in data.commands)
+    commands.push([task, data.commands[i]]);
+  req.db.get('hypervisor').updateOne(
+    {'ack': 0},
+    {'$push': {commands: commands}},
+    {upsert: true}
+  ).then( () => res.sendStatus(200))
+  .catch( (err) => res.status(200).json({message: err.message}));
+});
+
+router.post('/redax_control', ensureAuthenticated, function(req, res) {
+  var data = req.body.data;
+  var commands = [];
+  for (var i in data)
+    commands.push(['redax_ctl', data[i]]);
+  req.db.get('hypervisor').updateOne(
+    {'ack': 0},
+    {'$push': {commands: commands}},
+    {upsert: true}
+  ).then( () => {
+    return res.sendStatus(200);
+  }).catch( (err) => {
+    return res.status().json({message: err.message});
+  });
+});
+
+router.post('/vme_control', ensureAuthenticated, function(req, res) {
+  var data = req.body.data;
 });
