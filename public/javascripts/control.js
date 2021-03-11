19a3016d989173d31383ea9a6e71a8e1e9f35475
Darryl Masson <darrylmasson@gmail.com>
2021-03-11 14:59:12 +0100

    Extra tweaks

--- public/javascripts/control.js
+++ public/javascripts/control.js
@@ -1,17 +1,46 @@
 var initial_control = {};
 
 function DefineButtonRules(){
+  // handle LZ buttons first
+  $("#lz_remote").removeClass("det_control").removeClass("det_remote").change(function(){
+    document.page_ready = false;
+    if(!$("#lz_remote").is(":checked")){
+      alert("Local control of LZ is only available from SURF");
+      $("#lz_remote").bootstrapToggle('on');
+    }
+    document.page_ready = true;
+  });
+
+  $("#lz_active").removeClass("det_control").removeClass("det_active").change(function(){
+    document.page_ready = false;
+    if (!$("#lz_active").is(":checked")) {
+      alert("You can't stop LZ");
+      $("#lz_active").bootstrapToggle("on");
+    }
+    document.page_ready = true;
+  });
+
+  $("#lz_softstop").removeClass("det_control").removeClass("det_softstop").change(function(){
+    document.page_ready = false;
+    if($("#lz_softstop").is(":checked")){
+      alert("You want to go soft on LZ? I hope you aren't an AC");
+      $("#lz_softstop").bootstrapToggle('off');
+    }
+    document.page_ready = true;
+  });
 
   $(".det_control").change(function(){
-    if(document.page_ready == true)
+    if(document.page_ready == true) {
       $("#confirm_div").fadeIn("fast");
+      CheckLinking();
+    }
   });
 
   $(".det_remote").change(function() {
     if (document.page_ready) {
       document.page_ready = false;
       var det = this.id.substr(0, this.id.length-'_remote'.length);
-      if ($(`#{det}_remote`).is(":checked")) {
+      if ($(`#${det}_remote`).is(":checked")) {
         $(`#${det}_active`).bootstrapToggle('off');
       }
       SetRemote(det);
@@ -23,50 +52,21 @@ function DefineButtonRules(){
     if (document.page_ready) {
       document.page_ready = false;
       var det = this.id.substr(0, this.id.length-'_active'.length);
-      // If we're in remote mode, fail
+      // If we're in remote mode, fail. We shouldn't get here, but still
       if ($(`#${det}_remote`).is(":checked")) {
         alert(`You cannot control the ${det.replace('_',' ')} when it is in remote mode`);
         $(`#${det}_active`).bootstrapToggle('toggle');
-      } else
-        SetLinking();
+      }
       document.page_ready = true;
     }
   });
 
-  $(".det_mode").change(() => {
-    if(document.page_ready){
-      document.page_ready = false;
-      SetLinking();
-      document.page_ready = true;
-    }
-  });
-
-  $("#lz_remote").removeClass("det_control").removeClass("det_remote").change(function(){
-    if(!$("#lz_remote").is(":checked")){
-      alert("Local control of LZ is only available from SURF");
-      $("#lz_remote").bootstrapToggle('on');
-    }
-  });
-
-  $("#lz_active").removeClass("det_control").removeClass("det_active").change(function(){
-    if (!$("#lz_active").is(":checked")) {
-      alert("You can't stop LZ");
-      $("#lz_active").bootstrapToggle("on");
-    }
-  });
-
-  $("#lz_softstop").removeClass("det_control").removeClass("det_softstop").change(function(){
-    if($("#lz_softstop").is(":checked")){
-      alert("You want to go soft on LZ? I hope you aren't an AC");
-      $("#lz_softstop").bootstrapToggle('off');
-    }
-  });
 } // DefineButtonRules
 
 function SetRemote(detector){
   if (detector == 'lz') return;
   var is_remote = $(`#${detector}_remote`).is(":checked");
-  ['active', 'softstop', 'stop_after', 'comment'].forEach(att => $(`#${detector}_${att}`).prop('disabled', is_remote));
+  ['active', 'softstop', 'stop_after', 'comment', 'mode'].forEach(att => $(`#${detector}_${att}`).prop('disabled', is_remote));
   if (is_remote) {
     $(`#${detector}_active`).bootstrapToggle('off');
   } else {
@@ -74,11 +74,13 @@ function SetRemote(detector){
   }
 }
 
-function SetLinking() {
+function CheckLinking() {
   var detectors = ['tpc', 'muon_veto', 'neutron_veto'];
   var modes = detectors.map(det => $(`#${det}_mode`).val());
   var links = detectors.map(det => $(`#${det}_mode :selected`).attr("link_type").split(","));
   var active = detectors.map(det => $(`#${det}_active`).is(":checked"));
+  var remote = detectors.map(det => $(`#${det}_remote`).is(":checked"));
+  var softstop = detectors.map(det => $(`#${det}_softstop`).is(":checked"));
   var stopafter = detectors.map(det => $(`#${det}_stop_after`).val());
   var invalid = false;
   var is_linked = [[null, false, false], [null, null, false]];
@@ -87,7 +89,7 @@ function SetLinking() {
   for (var i = 0; i < detectors.length-1; i++) {
     for (var j = i+1; j < detectors.length; j++) {
       if (links[i].includes(detectors[j]) || links[j].includes(detectors[i]))
-        invalid ||= (modes[i] != modes[j] || active[i] != active[j] || stopafter[i] != stopafter[j]);
+        invalid ||= (modes[i] != modes[j] || active[i] != active[j] || stopafter[i] != stopafter[j] || softstop[i] != softstop[j] || remote[i] || remote[j]);
       is_linked[i][j] = links[i].includes(detectors[j]) && links[j].includes(detectors[i]);
     }
   }
