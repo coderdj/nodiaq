31e4e00938c2eb1269536f1cea4a70a29512f251
Florian Tönnies <T430>
2021-08-04 09:30:37 +0200

    added zylinder to 3d view

--- public/javascripts/monitor_scripts.js
+++ public/javascripts/monitor_scripts.js
@@ -45,7 +45,7 @@ var playback_interval = false
 var main_loop_interval = false
 
 
-var timer
+var timer = []
 var timer_ini
 
 var rate_total
@@ -356,9 +356,6 @@ function tpc_pos(array, coords){
 
 function calc_3d_pos(input_coords, calc){
     var coords_3d = []
-    var scaling_tpc_x = 1
-    var scaling_tpc_y = 1
-    var scaling_tpc_z = 1
     
     var y_offset = 0
     
@@ -370,16 +367,16 @@ function calc_3d_pos(input_coords, calc){
         switch(calc){
             case "top":
                 coords_3d = [
-                    input_coords[0] * scaling_tpc_x,
-                    input_coords[1] * scaling_tpc_y,
-                    140 * scaling_tpc_z,
+                    input_coords[0],
+                    input_coords[1],
+                    140+20
                 ]
                 break
             case "bottom":
                 coords_3d = [
-                    input_coords[0] * scaling_tpc_x,
-                    input_coords[1] * scaling_tpc_y,
-                    0 * scaling_tpc_z,
+                    input_coords[0],
+                    input_coords[1],
+                    0
                 ]
                 break
         }
@@ -666,7 +663,59 @@ function build_pmt_layouts(){
         
     }    
     }
-       
+    
+    
+    
+    {// 3D Deco
+        var tpc_real_radius = 125/2 // [cm]
+        var top_center = calc_3d_pos([0,0], "top")
+        var bot_center = calc_3d_pos([0,0], "bottom")
+        var tpc_radius = 2*((bot_center[1]-calc_3d_pos([0,tpc_real_radius], "bottom")[1]) + layout_style["3d"]["pmt_size"])
+        var scaling_y = .85
+        
+        
+        var bot_array_ellipse = document.createElementNS(svgns, 'ellipse');
+        bot_array_ellipse.setAttributeNS(null, 'cx', bot_center[0]);
+        bot_array_ellipse.setAttributeNS(null, 'cy', bot_center[1]);
+        bot_array_ellipse.setAttributeNS(null, 'rx', tpc_radius);
+        bot_array_ellipse.setAttributeNS(null, 'ry', tpc_real_radius * scaling_y);
+        bot_array_ellipse.setAttributeNS(null, 'class', "deco deco_3d deco_off");
+        bot_array_ellipse.setAttributeNS(null, 'style', "fill:none;stroke:grey;");
+        
+        var top_array_ellipse = document.createElementNS(svgns, 'ellipse');
+        top_array_ellipse.setAttributeNS(null, 'cx', top_center[0]);
+        top_array_ellipse.setAttributeNS(null, 'cy', top_center[1]);
+        top_array_ellipse.setAttributeNS(null, 'rx', tpc_radius);
+        top_array_ellipse.setAttributeNS(null, 'ry', tpc_real_radius * scaling_y);
+        top_array_ellipse.setAttributeNS(null, 'class', "deco deco_3d deco_off");
+        top_array_ellipse.setAttributeNS(null, 'style', "fill:none;stroke:grey;");
+        
+        var left_line = document.createElementNS(svgns, 'line');
+        left_line.setAttributeNS(null, 'x1', top_center[0]-tpc_radius);
+        left_line.setAttributeNS(null, 'x2', top_center[0]-tpc_radius);
+        left_line.setAttributeNS(null, 'y1', top_center[1]);
+        left_line.setAttributeNS(null, 'y2', bot_center[1]);
+        left_line.setAttributeNS(null, 'class', "deco deco_3d deco_off");
+        left_line.setAttributeNS(null, 'style', "stroke:grey;");
+        
+        var right_line = document.createElementNS(svgns, 'line');
+        right_line.setAttributeNS(null, 'x1', top_center[0]+tpc_radius);
+        right_line.setAttributeNS(null, 'x2', top_center[0]+tpc_radius);
+        right_line.setAttributeNS(null, 'y1', top_center[1]);
+        right_line.setAttributeNS(null, 'y2', bot_center[1]);
+        right_line.setAttributeNS(null, 'class', "deco deco_3d deco_off");
+        right_line.setAttributeNS(null, 'style', "stroke:grey");
+        
+
+
+        svgObject1.appendChild(bot_array_ellipse)
+        svgObject1.appendChild(top_array_ellipse)
+        svgObject1.appendChild(left_line)
+        svgObject1.appendChild(right_line)
+
+    
+    
+    }
     
     
     } // END of decorations 
